ee692f2562d83e7173b4f3b257e9c9b16f02c9de
sjulstad <43213803+sjulstad@users.noreply.github.com>
2021-12-11 21:49:37 -0700

    Add files via upload

---
+++ pyCoffee/script.js
@@ -0,0 +1,9 @@
+mapboxgl.accessToken = 'pk.eyJ1Ijoic2p1bHN0YWQiLCJhIjoiY2tuOGhxYjh5MDlvNzJucXY2ZmthM2NheCJ9.4O18jIcCi3h1aeEoPJG7Tw';
+    const map = new mapboxgl.Map({
+        container: 'map', // container ID
+        style: 'mapbox://styles/sjulstad/ckwzzmvcp1r9d15um9ggu3bi7', // style URL
+        center: [38.296699, 18.130011], // starting position [lng, lat]
+        zoom: 1.65 // starting zoom    
+});
+
+
